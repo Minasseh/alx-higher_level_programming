cb75373a20988ab41bb68285673806e72e2ce634
Minasseh <mnsalaro@yahoo.com>
2023-10-09 05:48:56 -0700

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_list_integer(my_list=[]):
     """Prints what is in my list"""
-    for l in range(len(my_list)):
-        print("{:d}".format(my_list[l]))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
