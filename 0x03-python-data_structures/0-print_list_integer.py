ba1a723b331405a9eaa048f8d3ea088abf57cab0
Minasseh <mnsalaro@yahoo.com>
2023-10-09 05:00:55 -0700

    Prints my list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@
 def print_list_integer(my_list=[]):
     """Prints what is in my list"""
     for l in range(len(my_list)):
-        print("\n{}".format(my_list[l]))
+        print("{}".format(my_list[l]))
