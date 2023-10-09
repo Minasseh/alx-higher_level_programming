f591fce46a8c54d9e8085ab64321598841af9d1e
Minasseh <mnsalaro@yahoo.com>
2023-10-09 08:25:35 -0700

    element at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+##!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """Prints elements at index idx"""
+    if idx < 0:
+        return
+    if idx > len(my_list):
+        return
+    for idx in range(len(my_list)):
+        print("Element at index {:d} is {:d}".format(idx, my_list(idx)))
